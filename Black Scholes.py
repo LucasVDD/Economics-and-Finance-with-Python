ccca97beca44582a80454f2095a29b226326d4c4
Lucas <vandenluc11@gmail.com>
2025-05-13 17:17:20 -0400

    Improved inputs

--- Black Scholes.py
+++ Black Scholes.py
@@ -5,29 +5,19 @@ import scipy.stats as sp
 
 # Declaring/Inputing Variables
 
-
-    ## **NEEDS TO BE REWORKED TO PREVENT STRINGS FROM PASSING**
-
 while True:
-
-    s = float(input("Enter the stock price at time t, S:"))
-    T = float(input("Enter the total time horizon, T:"))
-    t = float(input("Enter the time to expiry t:"))
-    k = float(input("Enter the Strike Price, k:"))
-    r = float(input("Enter the Risk-Free Rate, r:"))
-    sigma = float(input("Enter the volatility, sigma:"))
-    
-    if not all(isinstance(i, (int , float)) for i in (s , t , k , r , sigma)):
-        print("You entered a value incorrectly, try again (must be int or float)")
-        print(type(s),type(t),type(k),type(r),type(sigma))
-        continue
-    else:
-        #All variables must be integers or floats.
+    try:
+        s = float(input("Enter the stock price at time t, S:"))
+        k = float(input("Enter the Strike Price, k:"))
+        r = float(input("Enter the Risk-Free Rate, r:"))
+        sigma = float(input("Enter the volatility, sigma:"))
+        T = float(input("Enter the total time horizon, T:"))
+        t = float(input("Enter the time to expiry t:"))
         break
-
+    except ValueError:
+        print("Invalid input, please enter a float or string")
 
 # Calculating delta 1 and delta 2
-
 d1 = (math.log(s/k) + ((r + (sigma**2)/2) * (T-t)))/(sigma * np.sqrt(T-t))
 
 d2 = (math.log(s/k) + ((r - (sigma**2)/2) * (T-t)))/(sigma * np.sqrt(T-t))
